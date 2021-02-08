a9f78e41defc2dd6c207067b56d8484567b8aec5
Eray Erdin <eraygezer.94@gmail.com>
2021-02-09 01:31:45 +0300

    created altbinutils::app

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,4 @@
-pub mod app;
-pub mod path;
-
-// Copyright 2021 erayerdin
+// Copyright 2021 Eray Erdin
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
