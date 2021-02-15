54b585cf272a7854d9f1a020545dbc437fdba2fd
Eray Erdin <eraygezer.94@gmail.com>
2021-02-16 00:03:04 +0300

    fixed mod position

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,3 @@
-pub mod app;
-pub mod paths;
-
 // Copyright 2021 erayerdin
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,6 +11,9 @@ pub mod paths;
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+pub mod app;
+pub mod paths;
 
 pub enum ExitCodes {
     DirectoriesInitFailure,
