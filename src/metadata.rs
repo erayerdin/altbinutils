f6b8b2a07a38851e14f5473021608aeae479d826
Eray Erdin <eraygezer.94@gmail.com>
2021-10-11 10:14:10 +0300

    added metadata macro removed Metadata::default

--- src/metadata.rs
+++ src/metadata.rs
@@ -3,8 +3,6 @@ use std::fmt;
 use log::{debug, trace};
 use semver::Version;
 
-use crate::{error::ApplicationError, exit::CommonExitCodes, result::ApplicationResult};
-
 // Copyright 2021 Eray Erdin
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,6 +16,38 @@ use crate::{error::ApplicationError, exit::CommonExitCodes, result::ApplicationR
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+#[macro_export]
+macro_rules! metadata {
+    () => {{
+        (|| {
+            use semver::Version;
+
+            let name = env!("CARGO_PKG_NAME").to_owned();
+            let version = match Version::parse(env!("CARGO_PKG_VERSION")) {
+                Ok(v) => v,
+                Err(e) => {
+                    return Err(ApplicationError::InitError {
+                        exit_code: crate::exit::CommonExitCodes::SemverVersionParseFailure as i32,
+                        message: format!("Could not parse version of application. {}", e),
+                    });
+                }
+            };
+            let description = env!("CARGO_PKG_DESCRIPTION").to_owned();
+            let authors: Vec<String> = env!("CARGO_PKG_AUTHORS")
+                .split(";")
+                .into_iter()
+                .map(|s| s.trim().to_owned())
+                .collect();
+            Ok(Metadata {
+                name,
+                version,
+                description,
+                authors,
+            })
+        })()
+    }};
+}
 
 #[derive(Debug, Clone)]
 pub struct Metadata {
@@ -46,38 +76,5 @@ impl Metadata {
             description: description.into(),
             authors: authors.into_iter().map(|v| v.into()).collect(),
         }
-    }
-
-    pub fn default() -> ApplicationResult<Self> {
-        debug!("Initializing metadata from defaults...");
-
-        let name = env!("CARGO_PKG_NAME").to_owned();
-        let version = match Version::parse(env!("CARGO_PKG_VERSION")) {
-            Ok(v) => v,
-            Err(e) => {
-                return Err(ApplicationError::InitError {
-                    exit_code: CommonExitCodes::SemverVersionParseFailure as i32,
-                    message: format!("Could not parse version of application. {}", e),
-                });
-            }
-        };
-        let description = env!("CARGO_PKG_DESCRIPTION").to_owned();
-        let authors: Vec<String> = env!("CARGO_PKG_AUTHORS")
-            .split(";")
-            .into_iter()
-            .map(|s| s.trim().to_owned())
-            .collect();
-
-        trace!("name: {}", name);
-        trace!("version: {}", version);
-        trace!("description: {}", description);
-        trace!("authors: {:?}", authors);
-
-        Ok(Self {
-            name,
-            version,
-            description,
-            authors,
-        })
     }
 }
